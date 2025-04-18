cebffd27a00b317353a0ae6aed6427c52d1a94b4
AnandaCahya <tgigroupjap@gmail.com>
2025-04-18 19:18:38 +0700

    feat [landing_page]

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <div class="flex relative flex-col md:flex-row w-full h-full bg-[#090909] py-2">
+      <div class="w-full h-3/5 md:w-5/12 md:h-full flex flex-col justify-center items-center">
+        <div>
+          <p class="text-xl font-bold">Miyuna Developer</p>
+          <a>Multiplatform series list and tracking</a>
+        </div>
+      </div>
+      <div class="w-full h-2/5 md:w-7/12 md:h-full relative flex flex-col justify-center-safe items-center">
+        <div class="w-full h-full absolute top-0 left-0 bg-gradient-to-b md:bg-gradient-to-r from-[rgba(0,0,0,0.9)] to-[rgba(0,0,0,0.4)] ">
+        </div>
+        <img class="h-full md:h-4/6 object-cover" src="/src/assets/kiirohana_library.jpeg">
+      </div>
+    </div>
+
+    <div class="bg-[#23002c]">
+      <div class="p-8">
+        <div class="p-4 shadow-2xl">
+          <p class="text-center font-bold text-3xl my-2">What is Miyuna?</p>
+          <p class="md:text-lg p-10">We are a multiplatform Read & Watch Series/Novel/Comic app development team. You can enjoy the app for multiple platforms and variety of sweets that you can choose from</p>
+          
+          <div>
+            <p class="text-2xl font-semibold p-4 text-center">Do it all in one place</p>
+              <div class="w-full flex flex-col md:flex-row justify-center items-center">
+                <img class="w-full md:w-2/6 object-contain" src="/src/assets/kiirohana_library.jpeg">
+                <p class="w-full md:w-4/6 p-8 md:text-lg">We provide an app where you can read and watch the shows or series you follow in just one app and easy. Whether reading Manga, Light Novel or Web Novel then Watch your favorite Drama, Anime and Movies with your choice of language.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
