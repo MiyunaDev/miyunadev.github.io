f17b2fe6a76d843e8ea2466f33b2691fbc2fd4e6
AnandaCahya <tgigroupjap@gmail.com>
2025-07-21 11:31:43 +0700

    fix [icon] Eror show icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig,  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import Icons from 'unplugin-icons/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ vue(), tailwindcss(), Icons({ compiler: 'vue3' })],
+  plugins: [ vue(), tailwindcss()],
 })
